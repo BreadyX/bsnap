57b5f4d8b71f3c7f86428826ae0bff0bf53df2c7
BreadyX <alex.bradatan85@yahoo.it>
2020-03-14 16:42:27 +0100

    Added pretty line breaks for help 2-col entries (command and options) - Renamed print_line to print_entry to be more clear - Added pretty line breaks to functions using print_entry

--- src/cmd/help.c
+++ src/cmd/help.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
 #define __CMD_INTERNAL
 #include "cmd.h"
@@ -8,7 +10,8 @@ static int INDENT_SIZE = 2;
 static int FIRST_COLUMN_LEN = 20;
 static int WRAP_AT = 80;
 
-static void print_line(char *first, char *second);
+static void print_entry(char *first, char *second);
+static void new_line_second_col(void);
 
 void print_help_usage(b_cmd_context *context)
 {
@@ -28,15 +31,19 @@ void print_command_description(b_cmd_context *context)
 {
 	printf("Commands:\n");
 	for (int i = 0; !command_is_null(context->commands[i]); i++)
-		print_line(context->commands[i].name, context->commands[i].tip);
+		print_entry(context->commands[i].name, context->commands[i].tip);
 }
 
-void print_line(char *first, char *second)
+void print_entry(char *first, char *second)
 {
 	int first_len = strlen(first);
 	int second_len = strlen(second);
-	int remaining = WRAP_AT - (INDENT_SIZE + FIRST_COLUMN_LEN);
-
+	
+	int second_i, cur_word_i;
+	int second_size = WRAP_AT - (INDENT_SIZE + FIRST_COLUMN_LEN);
+	int remaining = second_size;
+	char word[STRLEN];
+	
 	// indent
 	for (int i = 0; i < INDENT_SIZE; i++)
 		putchar(' ');
@@ -44,29 +51,36 @@ void print_line(char *first, char *second)
 	// first
 	printf("%s", first);
 	if (first_len > FIRST_COLUMN_LEN) {
-		putchar('\n');
-		for (int i = 0; i < FIRST_COLUMN_LEN + INDENT_SIZE; i++)
-			putchar(' ');
+		new_line_second_col();
 	} else
 		for (int i = 0; i < FIRST_COLUMN_LEN - first_len; i++)
 			putchar(' ');
 
 	// second
 	if (second_len > remaining) {
-		for(int i = 0; i < remaining; i++)
-			putchar(second[i]);
-		putchar('\n');
-
-		// TODO: pretty linebreak
-		for (int base = remaining; base < second_len; base += remaining) {
-			for (int i = 0; i < FIRST_COLUMN_LEN + INDENT_SIZE; i++)
-				putchar(' ');
-			for(int i = 0; i < remaining; i++) {
-				if (base + i < second_len)
-					putchar(second[base + i]);
+		for(second_i = cur_word_i = 0; second_i <= second_len; second_i++) {
+			if (isblank(second[second_i]) || second[second_i] == '\0') {
+				word[cur_word_i] = '\0';
+				cur_word_i = 0;
+				if (second_i > remaining) {
+					remaining += second_size;
+					new_line_second_col();
+				}
+				printf("%s%c", word, second[second_i]);
+			} else {
+				if (cur_word_i < STRLEN)
+					word[cur_word_i] = second[second_i];
+				cur_word_i++;
 			}
-			putchar('\n');
 		}
+		putchar('\n');
 	} else
 		printf("%s\n", second);
+}
+
+void new_line_second_col(void)
+{
+	putchar('\n');
+	for (int i = 0; i < FIRST_COLUMN_LEN + INDENT_SIZE; i++)
+		putchar(' ');
 }
